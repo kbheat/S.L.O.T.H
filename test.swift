c4b2b0cd3a248868f9577ce120837b3361d56e0a
kbheat <KBHEAT168@GMAIL.COM>
2024-10-09 11:17:57 -0400

    test.swift

---
+++ test.swift
@@ -0,0 +1,14 @@
+import Foundation
+
+// A simple function that adds two integers
+func addNumbers(_ a: Int, _ b: Int) -> Int {
+    return a + b
+}
+
+// Test the function
+let number1 = 5
+let number2 = 10
+let result = addNumbers(number1, number2)
+
+// Print the result
+print("The sum of \(number1) and \(number2) is \(result).")
